4e57da626136f71673df91756386bb5a84ebfb07
joker53-1 <56860465+joker53-1@users.noreply.github.com>
2021-08-13 10:35:25 +0800

    update“
    git commit -am update“

---
+++ src/main/java/com/csun/mall/common/enums/CommonEnum.java
@@ -0,0 +1,10 @@
+package com.csun.mall.common.enums;
+
+public enum CommonEnum {
+
+    AUTHORIZATION("Authorization");
+    public final String value;
+    CommonEnum(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/csun/mall/service/BaseService.java
@@ -0,0 +1,20 @@
+package com.csun.mall.service;
+
+import com.csun.mall.web.response.PagedGridResult;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+public class BaseService {
+
+    public PagedGridResult setterPagedGrid(List<?> list, Integer page){
+        PageInfo<?> pageList=new PageInfo<>(list);
+        PagedGridResult grid=new PagedGridResult();
+        grid.setPage(page);
+        grid.setRows(list);
+        grid.setTotal(pageList.getPages());
+        grid.setRecords(pageList.getTotal());
+        return grid;
+    }
+
+}
