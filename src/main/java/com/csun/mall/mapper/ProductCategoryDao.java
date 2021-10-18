77ed076cc80784b0c616f7159606c7b51ca8b01d
joker53-1 <56860465+joker53-1@users.noreply.github.com>
2021-10-18 13:09:20 +0800

    修改优化

--- src/main/java/com/csun/mall/mapper/ProductCategoryDao.java
+++ src/main/java/com/csun/mall/mapper/ProductCategoryDao.java
@@ -4,10 +4,7 @@ import com.csun.mall.pojo.dto.ProductCategoryWithChildren;
 
 import java.util.List;
 
-/**
- * 商品分类自定义Dao
- * Created by macro on 2018/5/25.
- */
+
 public interface ProductCategoryDao {
     /**
      * 获取商品分类及其子分类
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -197,25 +197,6 @@ $(function () {
             password: $("#password").val(),
             code: $("#code").val()
         };
-        // alert(userBO.keyword)
-        // if (userBO.username == null || userBO.username == undefined || userBO.username == '') {
-        //     alert("用户名不能为空");
-        //     return;
-        // } else if (userBO.password == null || userBO.password == undefined || userBO.password == '') {
-        //     alert("密码不能为空");
-        //     return;
-        // }
-        // else if (userBO.email == null || userBO.email == undefined || userBO.email == '') {
-        //     alert("邮箱不能为空");
-        //     return;
-        // }
-        // else if (userBO.code == null || userBO.code == undefined || userBO.code == '') {
-        //     alert("验证码不能为空");
-        //     return;
-        // } else if (userBO.password.length < 6) {
-        //     alert("密码不能少于6位");
-        //     return;
-        // }
 
         // form提交
         $.post("/customer/passport/regist",userBO,function (data) {
@@ -232,6 +213,10 @@ $(function () {
             }
         })
         return false
+    })
+
+    $("#add_cart").click(function (){
+        alert("功能开发中！")
     })
 
 });
